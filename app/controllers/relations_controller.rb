7099922671e3990c71523a61bb1e8d20e513c2ad
Dorian Matić <dorian.matic@ingemark.com>
2022-04-01 19:08:39 +0200

    implemnted fix

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -7,7 +7,7 @@ class RelationsController < ApplicationController
     other_user = User.find(params[:user_id])
     @relation = Relation.new(follower_id: current_user.id, followee_id: other_user.id)
     @relation.save
-    Notification.create!(user_id: current_user.id, source_id: @relation.id, source_type: 'Relation')
+    Notification.create!(sender: current_user, receiver: other_user, source: @relation)
     redirect_back(fallback_location: root_path)
   end
 
--- db/migrate/20220324073917_create_notifications.rb
+++ db/migrate/20220324073917_create_notifications.rb
@@ -3,10 +3,14 @@
 class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :sender
+      t.references :recipient
       t.references :source, polymorphic: true
       t.boolean :read, default: false
       t.timestamps
     end
+
+    add_foreign_key :notifications, :users, column: :sender_id, primary_key: :id
+    add_foreign_key :notifications, :users, column: :recipient_id, primary_key: :id
   end
 end
